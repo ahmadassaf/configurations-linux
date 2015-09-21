d6d500b4c6c696213b6800262d26a0543e9c608c
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2015-09-21 18:51:04 +0200

    fixing colors to linux style

--- install.sh
+++ install.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-echo "\n ===========================================================
+printf "\n ===========================================================
             ${magenta}Installing Server Configurations${NC}
  =========================================================== \n"
 
 # Colors and visual Configurations
-export magenta='\033[35m'
-export red='\033[31m'
-export NC='\033[0m'
-export BLINK='\033[5m'
-export NORMAL='\033[0'
+export magenta='\e[0;35m'
+export red='\e[0;31m'
+export NC='\e[0m'
 
-echo "${magenta}Setting up SSH Installation...${NC}"
+printf "${magenta}Setting up SSH Installation...${NC}"
 
 if [[ -d ${HOME}/.oh-my-zsh ]]; then
 	read -p "Have you configured SSH? [Y/N] " -n 1;
@@ -25,7 +23,7 @@ if [[ -d ${HOME}/.oh-my-zsh ]]; then
 		ssh-add ~/.ssh/id_rsa
 
 		# Add your SSH key to your account
-		echo "${red}Please dont forget to add your id_rsa.pub key to Github${NC}"
+		printf "${red}Please dont forget to add your id_rsa.pub key to Github${NC}"
 	fi;
 fi
 
@@ -39,7 +37,7 @@ if [[ -d ${HOME}/.oh-my-zsh ]]; then
 fi
 
 function install() {
-	echo "${magenta}Cloning Required repositories...${NC}"
+	printf "${magenta}Cloning Required repositories...${NC}"
 
 	git clone -b linux "git@github.com:ahmadassaf/bash-it.git"
 	git clone -b linux "git@github.com:ahmadassaf/dotfiles.git"
@@ -47,5 +45,5 @@ function install() {
 }
 
 
-echo "${red}To configure SSH login without password please do that on your local machine: \n cat ~/.ssh/id_rsa.pub | ssh root@149.202.53.241 \"mkdir ~/.ssh; cat >> ~/.ssh/authorized_keys\"\n${NC}"
-echo "${red}You also need to configure the git config file with: Host 149.202.53.241\nUser root\nIdentityFile ~/.ssh/id_rsa\nPubkeyAuthentication yes\nPreferredAuthentications publickey\n${NC}"
+printf "${red}To configure SSH login without password please do that on your local machine: \n cat ~/.ssh/id_rsa.pub | ssh root@149.202.53.241 \"mkdir ~/.ssh; cat >> ~/.ssh/authorized_keys\"\n${NC}"
+printf "${red}You also need to configure the git config file with: Host 149.202.53.241\nUser root\nIdentityFile ~/.ssh/id_rsa\nPubkeyAuthentication yes\nPreferredAuthentications publickey\n${NC}"
