9f4e400636950be85dc7508507b7f446bac8ddd6
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2015-09-21 20:03:54 +0200

    adding post-install script

---
+++ post-install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+bash-it enable alias all
+bash-it enable plugins all
+bash-it enable completion all
+
+bash-it disable plugin chruby
+bash-it disable plugin chruby-auto
+bash-it disable plugin postgres
+bash-it disable plugin z
+bash-it disable plugin postgres
+bash-it disable plugin todo
+bash-it disable completion conda
+bash-it disable alias emacs
