35c41aef6f448920f98629c6dbae1be05b98e66b
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2015-09-21 18:52:20 +0200

    fixing unneeded f statements

--- install.sh
+++ install.sh
@@ -10,31 +10,27 @@ export magenta='\e[0;35m'
 export red='\e[0;31m'
 export NC='\e[0m'
 
-printf "${magenta}Setting up SSH Installation...${NC}"
+printf "${magenta}Setting up SSH Installation...\n${NC}"
 
-if [[ -d ${HOME}/.oh-my-zsh ]]; then
-	read -p "Have you configured SSH? [Y/N] " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		# Generate a new SSH key
-		ssh-keygen -t rsa 4096 -C "ahmad.a.assaf@gmail.com"
-		# Add your key to the ssh-agent
-		eval "$(ssh-agent -s)"
-		ssh-add ~/.ssh/id_rsa
+read -p "Have you configured SSH? [Y/N] " -n 1;
+echo "";
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	# Generate a new SSH key
+	ssh-keygen -t rsa 4096 -C "ahmad.a.assaf@gmail.com"
+	# Add your key to the ssh-agent
+	eval "$(ssh-agent -s)"
+	ssh-add ~/.ssh/id_rsa
 
-		# Add your SSH key to your account
-		printf "${red}Please dont forget to add your id_rsa.pub key to Github${NC}"
-	fi;
-fi
+	# Add your SSH key to your account
+	printf "${red}Please dont forget to add your id_rsa.pub key to Github${NC}"
+fi;
 
 
-if [[ -d ${HOME}/.oh-my-zsh ]]; then
-	read -p "Can you confirm that you added the public key to Github? [Y/N] " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		install
-	fi;
-fi
+read -p "Can you confirm that you added the public key to Github? [Y/N] " -n 1;
+echo "";
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	install
+fi;
 
 function install() {
 	printf "${magenta}Cloning Required repositories...${NC}"
@@ -45,5 +41,5 @@ function install() {
 }
 
 
-printf "${red}To configure SSH login without password please do that on your local machine: \n cat ~/.ssh/id_rsa.pub | ssh root@149.202.53.241 \"mkdir ~/.ssh; cat >> ~/.ssh/authorized_keys\"\n${NC}"
+printf "${red}To configure SSH login without password please do that on your local machine:\ncat ~/.ssh/id_rsa.pub | ssh root@149.202.53.241 \"mkdir ~/.ssh; cat >> ~/.ssh/authorized_keys\"\n${NC}"
 printf "${red}You also need to configure the git config file with: Host 149.202.53.241\nUser root\nIdentityFile ~/.ssh/id_rsa\nPubkeyAuthentication yes\nPreferredAuthentications publickey\n${NC}"
