60655e51c1ed5847d821430f488755ea3a839175
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2016-07-13 12:49:58 +0100

    print out the ssh key id_rsa.pub so that it can be easily copied

--- configure-ssh.sh
+++ configure-ssh.sh
@@ -15,5 +15,9 @@ if [[ $REPLY =~ ^[Nn]$ ]]; then
 	ssh-add ~/.ssh/id_rsa
 
 	# Add your SSH key to your account
-	printf "Please dont forget to add your id_rsa.pub key to Github"
+    echo "";
+    cat ~/.ssh/id_rsa.pub
+    echo "";
+
+	printf "Please dont forget to add your id_rsa.pub key [above] to Github before you continue"
 fi;
