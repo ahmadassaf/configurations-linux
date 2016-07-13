f390af638239661d969eca72284c4cde1c876003
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2016-07-13 13:19:03 +0100

    adding -y flag to bypasss installation prompt

--- configure-apache.sh
+++ configure-apache.sh
@@ -6,21 +6,21 @@ printf "Installing apache modules and configurations...\n"
 # Installing pagespeed module
 wget https://dl-ssl.google.com/dl/linux/direct/mod-pagespeed-stable_current_amd64.deb
 sudo dpkg -i mod-pagespeed-*.deb
-sudo apt-get -f install
+sudo apt-get -f -y install
 sudo service apache2 reload
 
 # installing security modules
-sudo apt-get install libapache2-modsecurity
+sudo apt-get install -y libapache2-modsecurity
 
 #installing Spamhaus
-sudo apt-get install libapache2-mod-spamhaus
+sudo apt-get install -y libapache2-mod-spamhaus
 
 # Installing mode-evasive and fail 2ban
-sudo apt-get install libapache2-mod-evasive
-sudo apt-get install fail2ban
+sudo apt-get install -y libapache2-mod-evasive
+sudo apt-get install -y fail2ban
 
 # Installing mod-qos
-sudo apt-get install libapache2-mod-qos
+sudo apt-get install -y libapache2-mod-qos
 
 # Enabling some vital Apache Modules
 sudo a2enmod rewrite
